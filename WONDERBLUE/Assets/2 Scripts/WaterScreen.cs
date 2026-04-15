28bef2a917dec67440b2fd3c7bc3d660c1ec1484
maximumvvoltage <146975484+maximumvvoltage@users.noreply.github.com>
2026-04-15 13:31:16 +0100

    The great restart of '26

--- WONDERBLUE/Assets/2 Scripts/WaterScreen.cs
+++ WONDERBLUE/Assets/2 Scripts/WaterScreen.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class WaterVolume : MonoBehaviour
+public class WaterScreen : MonoBehaviour
 {
     [Header("References")]
     public BoxCollider waterVolume;
